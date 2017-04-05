aafd66148e0ac8806c1a371f180f8c54b1b74830
Vinzenz Gregor Eck <vinzenz@eck-mail.eu>
2017-04-05 19:33:08 +0200

    Added Files

---
+++ read_gps.py
@@ -0,0 +1,18 @@
+import android
+
+droid = android.Android()
+droid.startLocating()
+print("reading GPS ...")
+event = droid.eventWaitFor('location',10000).result
+if event['name'] == "location":
+
+try: 
+	lat = str(event['data']['gps']['latitude'])
+	lng = str(event['data']['gps']['longitude'])
+except KeyError:
+	lat = str(event['data']['network']['latitude'])
+	lng = str(event['data']['network']['longitude'])
+	latlng = 'lat: ' + lat + ' lng: ' + lng
+print(latlng)
+
+droid.stopLocating()
